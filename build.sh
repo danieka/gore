f634a41c3857184eaece62c0c1d48596a2916fb8
Daniel Karlsson <daniel@flinker.se>
2020-09-05 10:11:16 +0200

    CI: Build to tar packages

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 mkdir -p releases
 
-GOOS=darwin GOARCH=amd64 packr build && mv ./gore ./releases/darwin-gore \
-  && GOOS=linux GOARCH=amd64 packr build && mv ./gore ./releases/linux-gore \
-  && GOOS=windows GOARCH=386 packr build && mv ./gore.exe ./releases/gore.exe \
-  && packr clean
+GOOS=darwin GOARCH=amd64 packr build
+tar -czvf ./releases/darwin-gore.tar.gz ./gore
+rm ./gore
+
+GOOS=linux GOARCH=amd64 packr build
+tar -czvf ./releases/linux-gore.tar.gz ./gore
+rm ./gore
+
+GOOS=windows GOARCH=386 packr build
+tar -czvf ./releases/gore-exe.tar.gz ./gore.exe
+rm ./gore.exe
+
+packr clean
